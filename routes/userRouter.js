909b393e28bd5ee03dc391bef059abd0a1af0f8d
Adarsh Gour <52098572+rudra2027@users.noreply.github.com>
2021-11-22 22:43:24 +0530

    Add files via upload

---
+++ routes/userRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const userCtrl = require('../controllers/userCtrl')
+const auth = require('../middleware/auth')
+router.post('/register', userCtrl.register)
+router.post('/login', userCtrl.login)
+router.get('/logout', userCtrl.logout)
+router.get('/infor', auth, userCtrl.getuser)
+router.get('/refresh_token', userCtrl.refreshToken)
+
+
+module.exports = router
